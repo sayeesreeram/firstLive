93a627152752b8e2446c5e453ef3a1e1c78849af
Sayee Sree Ram <sayeesreeramanicorn@gmail.com>
2022-10-05 15:49:02 +0530

    Style changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { makeStyles } from '@mui/styles';
 import Header from './Components/Header';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
 import { useSelector } from 'react-redux';
 import FirstSection from './Components/FirstSection';
 
--- src/Components/FirstSection.js
+++ src/Components/FirstSection.js
@@ -11,7 +11,8 @@ const useStyles = makeStyles({
         width: "100vw",
         alignItems: "center",
         marginTop: "35vh",
-        height: "54vh"
+        height: "54vh",
+        textAlign:"center" 
     }
 })
 
@@ -23,13 +24,15 @@ function FirstSection() {
     return (
         <div className={style.firstSectionContent}>
             <div>
-                <div style={{ color: "yellow", font: "40px Work Sans" }}>Hi, My Name is Sayee Sree Ram G</div>
+                <div style={{ color: "yellow", font: "50px Work Sans",fontWeight:"700" }}>Hi, My Name is Sayee Sree Ram G</div>
+                <div style={{textAlign:"center" }}>
                 <div style={{ marginTop: "3%", color: "white", font: "24px Work Sans" }}>A Software Developer with a passion in learning </div>
                 <div style={{ color: "white", font: "24px Work Sans" }}>and implementing </div>
-                <div style={{ display: "flex", marginTop: "3vh", justifyContent: "space-between" }}>
+                </div>
+                <div style={{ display: "flex", marginTop: "3vh", justifyContent: "space-between",width:"20%",marginLeft:"40%" }}>
                     <LinkedInIcon style={{ color: "white", fontSize: "35px", margin: "5px", cursor: "pointer" }} />
                     <GitHubIcon style={{ color: "white", fontSize: "35px", margin: "5px", cursor: "pointer" }} />
-                    <EmailIcon style={{ color: "white", fontSize: "35px", margin: "5px", cursor: "pointer" }} />
+                    <EmailIcon style={{ color: "white", fontSize: "35px", margin: "5px", cursor: "pointer"}} />
                 </div>
             </div>
         </div>
